5cbd406ee7c4b83402c1c0ef215378b02c443d66
Cody Reichert <cody@reichertbrothers.com>
2015-07-06 18:05:46 -0500

    (emacs) - add custom theme to org-page

--- emacs/.emacs.d/config/init-orgpage.el
+++ emacs/.emacs.d/config/init-orgpage.el
@@ -5,13 +5,19 @@
 
 ;;; Code:
 (require 'org-page)
-(setq op/repository-directory "/home/cody/workspace/org/blog")
+(setq op/repository-directory "~/workspace/blog/blog")
 (setq op/site-domain "http://codyreichert.github.io")
 (setq op/personal-github-link "https://github.com/CodyReichert")
 (setq op/site-main-title "The One True Blog")
-(setq op/site-sub-title "Emacs, programming, and whatever else comes to mind")
+(setq op/site-sub-title "Emacs, Programming, and Arch Linux")
 (setq op/personal-disqus-shortname "theonetrueblog")
-(setq op/theme 'mdo)
+
+(setq op/theme-root-directory "~/workspace/blog/themes")
+(setq op/theme 'cody)
+
+(setq op/item-cache nil)
+
+;; (setq op/theme 'mdo)
 
 (provide 'init-orgpage)
 ;;; init-orgpage.el ends here
