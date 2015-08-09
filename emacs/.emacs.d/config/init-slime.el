04bbeb03d9c78d85f5f09b578809ba712d17a289
Cody Reichert <cody@reichertbrothers.com>
2015-08-09 02:03:05 -0500

    [emacs] Add CL syntax highlighting in SLIME

--- emacs/.emacs.d/config/init-slime.el
+++ emacs/.emacs.d/config/init-slime.el
@@ -19,5 +19,27 @@
 (define-key slime-repl-mode-map (kbd "C-p") 'slime-repl-backward-input)
 (define-key slime-repl-mode-map (kbd "C-n") 'slime-repl-forward-input)
 
+
+(defvar slime-repl-font-lock-keywords lisp-font-lock-keywords-2)
+(defun slime-repl-font-lock-setup ()
+  (setq font-lock-defaults
+        '(slime-repl-font-lock-keywords
+         ;; From lisp-mode.el
+         nil nil (("+-*/.<>=!?$%_&~^:@" . "w")) nil
+         (font-lock-syntactic-face-function
+         . lisp-font-lock-syntactic-face-function))))
+
+(add-hook 'slime-repl-mode-hook 'slime-repl-font-lock-setup)
+
+(defadvice slime-repl-insert-prompt (after font-lock-face activate)
+  (let ((inhibit-read-only t))
+    (add-text-properties
+     slime-repl-prompt-start-mark (point)
+     '(font-lock-face
+      slime-repl-prompt-face
+      rear-nonsticky
+      (slime-repl-prompt read-only font-lock-face intangible))))))
+
+
 (provide 'init-slime)
 ;;; init-slime.el ends here.
