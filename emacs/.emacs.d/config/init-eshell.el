3d641ab922fa9d5f68ec3ff53961d8d026ee401f
Cody Reichert <cody@reichertbrothers.com>
2015-09-22 11:44:50 -0500

    [emacs] add `clear' command for eshell

--- emacs/.emacs.d/config/init-eshell.el
+++ emacs/.emacs.d/config/init-eshell.el
@@ -13,5 +13,10 @@
 (setq eshell-review-quick-commands nil)
 (setq eshell-smart-space-goes-to-end t)
 
+(defun eshell/clear ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (delete-region (point-min) (point-max))))
+
 (provide 'init-eshell)
 ;;; init-eshell.el ends here
