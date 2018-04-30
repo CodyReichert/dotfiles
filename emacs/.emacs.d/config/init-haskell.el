7efc7f89e9cf9067d7b3e4fbae163abf190bc24e
Cody Reichert <cody@reichertbrothers.com>
2018-04-30 13:13:30 -0500

    [emacs] Simplify init-haskell configuration

--- emacs/.emacs.d/config/init-haskell.el
+++ emacs/.emacs.d/config/init-haskell.el
@@ -4,95 +4,32 @@
 ;;; haskell-mode settings
 
 ;;; Code:
-(require 'haskell)
-(require 'haskell-commands)
 (require 'haskell-mode)
-(require 'flycheck)
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(setq haskell-process-reload-with-fbytecode nil
+      haskell-process-use-presentation-mode t
+      haskell-process-type 'stack-ghci)
 
-(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(require 'flycheck-stack)
 
-;; (eval-after-load 'haskell-mode
-;;   '(define-key haskell-indentation-mode-map (kbd "<backtab>") nil))
+(add-hook 'haskell-mode-hook
+          (lambda ()
+            (interactive)
+            (progn
+              (haskell-indentation-mode)
+              (turn-on-haskell-doc)
+              (turn-on-haskell-decl-scan)
+              (interactive-haskell-mode)
+              (define-key haskell-mode-map (kbd "C-c C-;") 'haskell-process-load-file)
+              (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-reload)
+              (define-key haskell-mode-map (kbd "C-c i") 'haskell-navigate-imports-go)
+              (define-key haskell-mode-map (kbd "C-c I") 'haskell-navigate-imports-return)
+              (flycheck-select-checker 'haskell-stack-ghc)
+              )))
 
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-
-(setq haskell-interactive-popup-errors 'nil)
-
-(setq haskell-process-type 'stack-ghci)
-
-(setq haskell-process-use 'cabal-repl)
-
-
-(defun haskell-run-function-under-cursor ()
-"Send the `word-at-point' as a function to GHCi process."
-  (interactive)
-  (haskell-process-send-string
-   (haskell-interactive-process)
-   (format "%s" (word-at-point))))
-
-(define-key haskell-mode-map (kbd "C-c C-j") 'haskell-run-function-under-cursor)
-
-
-(flycheck-define-checker haskell-stack
-  "A Haskell syntax and type checker using ghc."
-  :command ("stack" "ghc" "--" "-Wall" "-fno-code" "-XTemplateHaskell"
-            (option-flag "-no-user-package-db"
-                         flycheck-ghc-no-user-package-database)
-            (option-list "-package-db" flycheck-ghc-package-databases)
-            (option-list "-i" flycheck-ghc-search-path concat)
-            ;; Include the parent directory of the current module tree, to
-            ;; properly resolve local imports
-            (eval (concat
-                   "-i"
-                   (flycheck-module-root-directory
-                    (flycheck-find-in-buffer flycheck-haskell-module-re))))
-            (option-list "-X" flycheck-ghc-language-extensions concat)
-            (eval flycheck-ghc-args)
-            "-x" (eval
-                  (pcase major-mode
-                    (`haskell-mode "hs")
-                    (`literate-haskell-mode "lhs")))
-            source)
-  :error-patterns
-  ((warning line-start (file-name) ":" line ":" column ":"
-            (or " " "\n    ") "Warning:" (optional "\n")
-            (message
-             (one-or-more " ") (one-or-more not-newline)
-             (zero-or-more "\n"
-                           (one-or-more " ")
-                           (one-or-more not-newline)))
-            line-end)
-   (error line-start (file-name) ":" line ":" column ":"
-          (or (message (one-or-more not-newline))
-              (and "\n"
-                   (message
-                    (one-or-more " ") (one-or-more not-newline)
-                    (zero-or-more "\n"
-                                  (one-or-more " ")
-                                  (one-or-more not-newline)))))
-          line-end))
-  :error-filter
-  (lambda (errors)
-    (flycheck-sanitize-errors (flycheck-dedent-error-messages errors)))
-  :modes (haskell-mode literate-haskell-mode)
-  :next-checkers ((warning . haskell-hlint)))
-
-
-(defun haskell-mode-setup-hook ()
-  "Disable the `haskell-ghc' check."
-  (interactive)
-  (progn
-    (turn-on-haskell-indent) ; haskell stuff
-    (turn-on-haskell-doc)
-    (turn-on-haskell-decl-scan)
-    (interactive-haskell-mode)
-    (flycheck-select-checker 'haskell-stack)))
-
-
-(add-hook 'haskell-mode-hook 'haskell-mode-setup-hook)
-
+(add-hook 'haskell-interactive-mode-hook
+          (lambda ()
+            (setq compilation-skip-threshold 2)))
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
