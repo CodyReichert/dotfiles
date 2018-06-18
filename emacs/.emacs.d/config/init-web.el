8bacc4478119172a43b0e4a0bfb9d0572caf7697
Cody Reichert <cody@reichertbrothers.com>
2018-06-18 15:37:43 -0500

    [emacs] simplify web config

--- emacs/.emacs.d/config/init-web.el
+++ emacs/.emacs.d/config/init-web.el
@@ -4,25 +4,16 @@
 ;;; web-mode settings
 
 ;;; Code:
+
 (require 'web-mode)
 (require 'flycheck)
 
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;;; Web-mode setup
 
 (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
 
 (setq web-mode-content-types-alist
   '(("jsx" . "\\.js[x]?\\'")))
-
-(setq-default flycheck-disabled-checkers
-              (append flycheck-disabled-checkers
-                      '(javascript-jshint)))
-
-(flycheck-add-mode 'javascript-eslint 'web-mode)
-
-(setq-default flycheck-disabled-checkers
-              (append flycheck-disabled-checkers
-                          '(json-jsonlist)))
 
 (defun web-mode-indent-hook ()
   "Hooks for Web mode.  Adjust indent."
@@ -32,6 +23,16 @@
 
 (add-hook 'web-mode-hook  'web-mode-indent-hook)
 (add-hook 'web-mode-hook  'emmet-mode)
+
+;;; Flycheck setup
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint json-jsonlist)))
+
+(flycheck-add-mode 'javascript-eslint 'web-mode)
 
 (provide 'init-web)
 ;;; init-web.el ends here
