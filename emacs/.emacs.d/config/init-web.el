1be268fcdcd67255b6b9c0e5932b638752369eca
Cody Reichert <cody@reichertbrothers.com>
2018-06-24 22:40:45 -0500

    [emacs] Update init-web.el
    We're removing our .dir-locals from our individual projects to just
    have what we need in our configurations.
    
    This addition makes flycheck use the `eslint` binary from the current
    file's closest node_modules. This is so it uses the correct plugin,
    configuration, etc and doesn't rely on a global eslint.

--- emacs/.emacs.d/config/init-web.el
+++ emacs/.emacs.d/config/init-web.el
@@ -34,5 +34,18 @@
 
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 
+(defun use-eslint-from-node-modules ()
+  "Find the eslint binary local to the current file to use the correct configuration, plugins, etc."
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/.bin/eslint"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook 'use-eslint-from-node-modules)
+
 (provide 'init-web)
 ;;; init-web.el ends here
